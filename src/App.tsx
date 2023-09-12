5866ff7f109c82a46c5a63e681ee519f2cb3538f
EminDege <emin.dege@gmail.com>
2023-09-12 15:22:33 +0300

    Projeye başlandı

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+//filtre denemesine başlıyorumadwad
+
+const GET_POKEMON_NAMES = gql`
+  query GetPokemonNames($limit: Int) {
+    pokemon_v2_pokemon(limit: $limit) {
+      name
+      height
+      weight
+      base_experience
+    }
+  }
+`;
+interface Pokemon {
+  id: string;
+  name: string;
+}
 
 function App() {
+  const { loading, error, data } = useQuery<{ pokemon_v2_pokemon: Pokemon[] }>(GET_POKEMON_NAMES, {
+    variables: { limit: 10 },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const pokemonList = data?.pokemon_v2_pokemon || [];
+  console.log(pokemonList);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokémon Names</h1>
+      <ul>
+        {pokemonList.map((pokemon: Pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
